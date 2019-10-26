cde555944ef39014f72edf46d5fd00b5222f7e5e
Davidsijj <curcadavid2@gmail.com>
2019-10-26 23:51:09 +0200

    oof

--- Blink.ino
+++ Blink.ino
@@ -1,11 +1,11 @@
-#define echoPin 2 // Echo Pin (OUTPUT pin in RB URF02)
-#define trigPin 3 // Trigger Pin (INPUT pin in RB URF02)
+#define echoPin 2 
+#define trigPin 3 
 
 int led1 = 5;
 int led2 = 4;
-int maximumRange = 350; // Maximum range needed
-int minimumRange = 0; // Minimum range needed
-long duration, distance; // Duration used to calculate distance
+int maximumRange = 350;
+int minimumRange = 0;
+long duration, distance
 int brightness;
 
 long previousMillis = 0;
@@ -21,15 +21,13 @@ void setup() {
 }
 
 void loop() {
-// The following trigPin/echoPin cycle is used to determine the distance of the nearest object by bouncing soundwaves off of it.
  digitalWrite(trigPin, LOW);
  delayMicroseconds(2);
  digitalWrite(trigPin, HIGH);
  delayMicroseconds(10);
  digitalWrite(trigPin, LOW);
  duration = pulseIn(echoPin, HIGH);
- distance = duration/58.2; //Calculate the distance (in cm) based on the speed of sound.
- //Serial.println(distance); // distance in cm
+ distance = duration/58.2;
  brightness= map(distance,1,100,0,255);
  if(distance <= 10){
   closeReadings++;
